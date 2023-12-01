de0af7953a4b45303d1708416bc518f009b46f59
Dylan <152401492+chops-07-chum@users.noreply.github.com>
2023-12-01 15:35:29 +0600

    Create Weather.java

---
+++ Weather.java
@@ -0,0 +1,12 @@
+// Weather.java
+public class Weather {
+    private String city;
+    private double temperature;
+    private String conditions;
+
+    public Weather(String city, double temperature, String conditions) {
+        this.city = city;
+        this.temperature = temperature;
+        this.conditions = conditions;
+    }
+}
